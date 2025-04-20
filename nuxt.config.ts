69f0ae0c21c33a267207a7568e463400d93777d3
Matvey M <kacill1@mail.ru>
2025-04-20 21:23:52 +0500

    some changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       firebase: {
-        apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY || 'mock_key',
+        apiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
         projectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
         storageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
